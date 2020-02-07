0da95bbd9e4380c4e45c15d5614faeb74268fd52
xiaozao1208 <lijuanshy@163.com>
2020-02-07 12:20:32 +0800

    ‘â2月6日更新

---
+++ chapter9/src/main/java/com/learn/httpclient/MyHttpClient.java
@@ -0,0 +1,26 @@
+package com.learn.httpclient;
+
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class MyHttpClient {
+
+    @Test
+    public void test1() throws IOException{
+        //用来存放我的结果
+        String result;
+        HttpGet get =new HttpGet("http://www.baidu.com");
+        HttpClient client = new DefaultHttpClient();
+        //执行get
+        HttpResponse response = client.execute(get);
+        result = EntityUtils.toString(response.getEntity(),"utf-8");
+        System.out.println(result);
+    }
+}
